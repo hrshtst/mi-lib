623037297bc583ebd376e1edd0f8b5bbac752f56
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-30 23:07:01 +0900

    fix: Update scripts to stop immediately when error occurs

--- build_compile_commands.sh
+++ build_compile_commands.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 . "liblist"
 JSON="$(pwd)/compile_commands.json"
 make clean
 for lib in $LIBS; do
+  echo "==> Processing $lib"
   cd "$lib" || continue
+  echo "Checking out branch 'compat/bear'..."
   git fetch origin compat/bear >/dev/null 2>&1
   git checkout compat/bear 2>/dev/null || {
     echo "❌  Failed to checkout 'compat/bear' in $lib"
@@ -21,6 +25,14 @@ cd ../joystick || exit
 bear --append --output "$JSON" -- make
 cd ../.. || exit
 compdb -p . list | sponge compile_commands.json
+
+for lib in $LIBS; do
+  cd "$lib" || continue
+  git checkout main 2>/dev/null || {
+    echo "❌  Failed to checkout 'main' in $lib"
+  }
+  cd ..
+done
 
 # Local Variables:
 # jinx-local-words: "compat env json liblist usr"
--- clone.sh
+++ clone.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # List of libraries
 . "liblist"
 OWNER="hrshtst"
