a6031e6aafecb8abce273b24321c96a18d98999e
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-30 23:27:29 +0900

    fix: Fix script to install commands

--- build_compile_commands.sh
+++ build_compile_commands.sh
@@ -9,7 +9,7 @@ for lib in $LIBS; do
   echo "==> Processing $lib"
   cd "$lib" || continue
   echo "Checking out branch 'compat/bear'..."
-  git fetch origin compat/bear >/dev/null 2>&1
+  # git fetch origin compat/bear >/dev/null 2>&1
   git checkout compat/bear 2>/dev/null || {
     echo "❌  Failed to checkout 'compat/bear' in $lib"
   }
@@ -18,12 +18,19 @@ done
 
 bear -- make
 cd pedi2/test || exit
+if [ ! -d gtest ]; then
+  unzip archive/gtest-1.7.0.zip
+  mv gtest-1.7.0 gtest
+fi
 bear --append --output "$JSON" -- make
 cd ../app/dynmorph || exit
 bear --append --output "$JSON" -- make
 cd ../joystick || exit
 bear --append --output "$JSON" -- make
 cd ../.. || exit
+if ! command -v sponge &> /dev/null; then
+  sudo apt install moreutils
+fi
 compdb -p . list | sponge compile_commands.json
 
 for lib in $LIBS; do
