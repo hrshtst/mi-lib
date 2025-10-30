2cc60823c069eb943483d6ae00aa3bc486a7db6b
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-30 22:18:27 +0900

    feat: Add script to re-build compile_commands.json

---
+++ build_compile_commands.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+. "liblist"
+JSON="$(pwd)/compile_commands.json"
+make clean
+for lib in $LIBS; do
+  cd "$lib" || continue
+  git fetch origin compat/bear >/dev/null 2>&1
+  git checkout compat/bear 2>/dev/null || {
+    echo "❌  Failed to checkout 'compat/bear' in $lib"
+  }
+  cd ..
+done
+
+bear -- make
+cd pedi2/test || exit
+bear --append --output "$JSON" -- make
+cd - || exit
+compdb -p . list | sponge compile_commands.json
+
+# Local Variables:
+# jinx-local-words: "compat env json liblist usr"
+# End:
