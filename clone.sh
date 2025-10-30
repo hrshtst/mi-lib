7605bf2383d255fd09856c5c9bdfbb6b7803e35b
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-30 22:30:29 +0900

    fix: Install dependencies in clone.sh

--- clone.sh
+++ clone.sh
@@ -4,6 +4,12 @@
 . "liblist"
 OWNER="hrshtst"
 
+echo "installing required packages..."
+### (for download & compilation)
+sudo apt install git wget unzip rename make gcc fakeroot pkg-config
+### (for ZEDA & X11 & OpenGL)
+sudo apt install libxml2-dev liblzf-dev xorg-dev libxft-dev libfreetype-dev libtiff-dev libjpeg-dev libmagickwand-dev freeglut3-dev libglew-dev libglfw3-dev
+
 for lib in $LIBS; do
     echo "==> Cloning $lib"
     if [ -d "$lib" ]; then
